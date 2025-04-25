184a1811041ac9a0bc650f6bdf3193d36b363f8a
Saman Jejo <saman_jejo@hotmail.com>
2025-04-25 16:32:26 +0200

    test

---
+++ PlanTrip/src/main/java/com/example/PlanTrip/PlanTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlanTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanTripApplication.class, args);
+	}
+
+}
---
+++ PlanTrip/src/test/java/com/example/PlanTrip/PlanTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlanTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
